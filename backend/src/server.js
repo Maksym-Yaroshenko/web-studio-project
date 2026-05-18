c6072043d25c027228e9b4ffab8fea2f73281e73
Maksym Yaroshenko <yiaroshenko.max@gmail.com>
2026-05-18 14:43:46 +0300

    Lab 5

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Глобальні middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 // Підключення ендпоінтів авторизації
